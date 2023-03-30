8bdfe4beb232b40272c34068b053a66f8da9bb0c
eda1-fiunam <francisco.rodriguez@ingenieria.unam.edu>
2023-03-30 14:50:58 -0600

    First commit. Funcional

---
+++ List.c
@@ -0,0 +1,195 @@
+
+#include "List.h"
+
+static Node* new_node( int data )
+{
+   Node* n = (Node*) malloc( sizeof( Node ) );
+   if( n != NULL )
+   {
+      n->data = data;
+
+      n->next = NULL;
+      n->prev = NULL;
+   }
+
+   return n;
+}
+
+
+List* List_New()
+{
+   List* lst = (List*) malloc( sizeof(List) );
+   if( lst )
+   {
+      lst->first = lst->last = lst->cursor = NULL;
+   }
+
+   return lst;
+}
+
+void List_Delete( List** p_list )
+{
+   assert( *p_list );
+
+   while( (*p_list)->first )
+   {
+      List_Pop_back( *p_list );
+   }
+
+   free( *p_list );
+   *p_list = NULL;
+}
+
+void List_Push_back( List* list, int item )
+{
+   Node* n = new_node( item );
+   assert( n );
+
+   if( list->first != NULL )
+   {
+      list->last->next = n;
+      n->prev = list->last;
+      list->last = n;
+   }
+   else
+   {
+      list->first = list->last = list->cursor = n;
+   }
+}
+
+void List_Pop_back( List* list )
+{
+   assert( list->first );
+   // ERR: no podemos borrar nada de una lista vacía
+
+   if( list->last != list->first )
+   {
+      Node* x = list->last->prev;
+      free( list->last );
+      x->next = NULL;
+      list->last = x;
+   }
+   else
+   {
+      free( list->last );
+      list->first = list->last = list->cursor = NULL;
+   }
+
+}
+
+
+
+bool List_Is_empty( List* list )
+{
+   return !list->first;
+}
+
+/**
+ * @brief Busca la primer ocurrencia con la llave key y si la encuentra coloca ahí al cursor.
+ *
+ * @param list Referencia a una lista.
+ * @param key La llave de búsqueda.
+ *
+ * @return true si encontró el elemento; false en caso contrario.
+ * @post Si hubo una ocurrencia el cursor es movido a esa posición; en caso contrario el cursor no se mueve.
+ */
+bool List_Find( List* list, int key )
+{
+   Node* start = list->first;
+   while( start )
+   {
+      if( start->data == key )
+      {
+         list->cursor = start;
+         return true;
+      }
+
+      start = start->next;
+   }
+   return false;
+}
+
+bool List_Remove( List* list, int key )
+{
+   // terminar
+   return false;
+}
+
+void List_Cursor_front( List* list )
+{
+   list->cursor = list->first;
+}
+
+void List_Cursor_back( List* list )
+{
+   list->cursor = list->last;
+}
+
+bool List_Cursor_next( List* list )
+{
+#if 0 
+   if( !list->cursor->next )
+   {
+      list->cursor = list->cursor->next;
+      return false;
+   }
+   else
+   {
+      list->cursor = list->first;
+      return true;
+   }
+#endif  
+   list->cursor = list->cursor->next;
+   return list->cursor;
+}
+
+bool List_Cursor_prev( List* list )
+{
+   // terminar
+   return false;
+}
+
+bool List_Cursor_end( List* list )
+{
+   return list->cursor;
+}
+
+int  List_Cursor_get( List* list )
+{
+   assert( list->cursor );
+
+   return list->cursor->data;
+}
+
+/**
+ * @brief Elimina el elemento apuntado por el cursor.
+ *
+ * @param list Referencia a una lista.
+ *
+ * @pre El cursor debe apuntar a una posición válida.
+ * @post El cursor queda apuntando al elemento a la derecha del elemento eliminado; si
+ * este hubiese sido el último, entonces el cursor apunta al primer elemento de la lista.
+ */
+void List_Cursor_erase( List* list );
+
+
+/**
+ * @brief Aplica la función fn() a cada elemento de la lista. La función fn() es una función unaria.
+ *
+ * @param list Una lista.
+ * @param fn Función unaria que será aplicada a cada elemento de la lista.
+ */
+void List_For_each( List* list, void (*fn)( int ) )
+{
+   Node* it = list->first;
+   // |it| es la abreviación de "iterator", o  en español, "iterador"
+
+   while( it != NULL )
+   {
+      fn( ( it->data ) );
+
+      it = it->next;
+   }
+}
+
+
